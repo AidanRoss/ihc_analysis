7e29447822ee69448bb1ebfc94272fb877b418c8
AidanRoss <aidan.ross@ndm.ox.ac.uk>
2016-08-16 11:38:36 +0100

    path changes

--- parameters.py
+++ parameters.py
@@ -5,7 +5,7 @@ def path():
 
     # 'path to extracted images'  # This should be the path to the folder containing the images
     extracted_images = '/Users/aidan/Desktop/aidan_summer/Week_Tasks/Week_9/test'
-
+    # extracted_images = '/Users/engs1348/Raquel/Nottingham-TMAs/tma-extracted'  # Raquel Path
     return extracted_images
 
 
@@ -13,6 +13,7 @@ def save():
 
     # 'path to save the images'  # This should be the path where you would like images to be saved
     save_path = '/Users/aidan/Desktop/aidan_summer/Week_Tasks/Week_9/save_images/segment2/'
+    # save_path = '/Users/engs1348/Raquel/githubRepositoryWorkingFiles/Histology_Aidan' # Raquel Path
 
     return save_path
 
@@ -27,7 +28,8 @@ def params():
 
 def microns():
 
-    # This value should be the pixel resolution of the images be ing analyzed
+    # This value should be the pixel resolution of the images being analyzed
+    # If the images are extracted at the same resolution as the scanner then the pixel res is 0.0456 mpp-x
     pixel_res = 0.0456  # microns per pixel_x
     pixel_size = pixel_res**2
 
@@ -39,4 +41,8 @@ def csv_save():
     # 'path to the csv file which contains the output data'
     csv_path = '/Users/aidan/Desktop/aidan_summer/Week_Tasks/Week_9'
     micron_csv = '/Users/aidan/Desktop/aidan_summer/Week_Tasks/Week_9'
+
+    # csv_path = '/Users/engs1348/Raquel/githubRepositoryWorkingFiles/Histology_Aidan'    # Raquel Path
+    # micron_csv = '/Users/engs1348/Raquel/githubRepositoryWorkingFiles/Histology_Aidan'  # Raquel Path to new csv
+
     return csv_path, micron_csv
